9c909bdbbcc26945a82ed3bb652c84fd748d4aff
rjak <rahuljainajaykumar@gmail.com>
2023-01-26 10:03:39 -0800

    Create automate_google.py

---
+++ automate_google.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+
+# Start a webdriver (e.g. Chrome)
+driver = webdriver.Chrome()
+
+# Navigate to the Google search page
+driver.get("https://www.google.com")
+
+# Find the search box element and enter a search query
+search_box = driver.find_element_by_name("q")
+search_box.send_keys("Selenium automation")
+
+# Find the search button element and click it
+search_button = driver.find_element_by_name("btnK")
+search_button.click()
+
+# Wait for the search results to load
+driver.implicitly_wait(10)
+
+# Print the title of the search results page
+print(driver.title)
+
+# Close the browser
+driver.close()
